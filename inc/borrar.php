2989e2f886f3c06b2eb61aa9a0d1a66d7cf0ec09
Sergio Alegre <sergio_alegre@hotmail.com>
2014-12-02 21:48:08 +0100

    Cerrar sesión

--- inc/borrar.php
+++ inc/borrar.php
@@ -1,13 +1,5 @@
-<?php require_once('../Connections/conexion.php'); ?>
-<?php
+<?php require_once('../Connections/conexion.php'); 
 
 
-mysql_select_db($database_conexion, $conexion);
-$query_Recordset1 = "SELECT * FROM z_datos";
-$Recordset1 = mysql_query($query_Recordset1, $conexion) or die(mysql_error());
-$row_Recordset1 = mysql_fetch_assoc($Recordset1);
-$totalRows_Recordset1 = mysql_num_rows($Recordset1);
-
-
-mysql_free_result($Recordset1);
 ?>
+Cerrar
--- inc/menu.php
+++ inc/menu.php
@@ -3,7 +3,7 @@
     <div id="item_me">Staff</div>
     <div id="item_me">Tops <img class="arrow" src="<?php echo $urlWeb ?>img/arrows-down.png" width="9" height="5" /></div>
     <div id="item_me">Crear post</div>
-    <div id="item_op"><img src="<?php echo $urlWeb ?>img/out.png" width="16" height="16" /></div>
+    <div id="item_op"><a href="<?php echo $logoutAction ?>"><img src="<?php echo $urlWeb ?>img/out.png" width="16" height="16" /></a></div>
     <div id="item_op" style="margin-top:1px"><?php echo nombre($_SESSION['MM_Id']);?></div>  <!-- invocamos la funcion user con la variable de sesion que tiene el id de usuario -->
     <div id="item_op"><img src="<?php echo $urlWeb ?>img/fav.png" width="16" height="16" /></div>
     <div id="item_op"><div id="notifica_msn">3</div><img  src="<?php echo $urlWeb ?>img/msn.png" width="16" height="16" /></div>
